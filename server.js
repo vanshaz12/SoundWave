99d64417661d4136129b952473d800e4af8ca5be
Vanshaz12 <vshmstr826@gmail.com>
2023-06-01 10:24:26 +0930

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("first_test")
+const express = require('express')
+const logger = require('./middlewares/logger')
+const sessions = require('./middlewares/session')
+
+const app = express()
+const port = process.env.PORT || 3001
+
+
