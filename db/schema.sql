0d577e9da4237e816594e5110dff6d6371c0471c
Vanshaz12 <vshmstr826@gmail.com>
2023-06-01 10:05:03 +0930

    schema-sql

---
+++ db/schema.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE sound_wave;
+\c sound_wave
+
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  email TEXT,
+  password_digest TEXT
+);
+
+CREATE TABLE songs(
+    song_id SERIAL PRIMARY KEY,
+    name TEXT,
+    artist TEXT,
+    duration INT
+);
+
+CREATE TABLE playlists(
+    playlist_id SERIAL PRIMARY KEY,
+    name TEXT,
+    user_id REFERENCES users(id)
+);
