3dbd5d88d8ba62333e6eeae012e074e71ce66024
Joseph <joseph.nijo.2001@gmail.com>
2023-06-01 14:11:55 +0930

    login done

---
+++ client/js/initialize.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+
+fetch('/api/sessions')
+  .then(res => res.json())
+  .then(data => {
+    if (data.result === 'successful') {
+      state.loggedInUser = data.email
+    }
+  })
