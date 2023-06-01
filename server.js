893835b8780e282905e067847080d655106f4c63
Vanshaz12 <vshmstr826@gmail.com>
2023-06-01 14:43:40 +0930

    server.js

--- server.js
+++ server.js
@@ -6,38 +6,33 @@ const sessions = require('./middlewares/session')
 
 const playlistController = require('./controllers/playlist_controllers')
 const usersController = require('./controllers/users_controller')
+const sess\
 
 const app = express()
 const port = process.env.PORT || 3001
 app.listen(port, () => 
     console.log(`Server running on port http://localhost:${port}`))
 
+// receieve request (from browser)
+//     |
+//     V
+// middleware function to log request info in the terminal
 app.use(logger)
+//     |
+//     V
+// middleware to send back to SPA (single page application)
 app.use(express.static('client'))
+//     |
+//     V
 app.use(express.json())
-app.use
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//     |
+//     V
+// enable sessions
+//     |
+//     V
+app.use(sessions)
+app.use('/api/playlist', playlistController)
 app.use('/api/users', usersController)
+app.use('/api/sessions', sessionsController)
 
 
