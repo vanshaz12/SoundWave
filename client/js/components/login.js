f69cd92e299c4fa940d155d3a5848497e69e043f
Vanshaz12 <vshmstr826@gmail.com>
2023-06-05 22:35:29 +0930

    nav-menu

--- client/js/components/login.js
+++ client/js/components/login.js
@@ -1,3 +1,5 @@
+
+
 function renderLogin() {
   document.querySelector('#page').innerHTML = `
     <section class='log-in'>
@@ -17,20 +19,48 @@ function renderLogin() {
   `
 }
 function logIn(event) {
-  event.preventDefault()
-  const form = event.target
-
-  const data = Object.fromEntries(new FormData(form))
+  event.preventDefault();
+  const form = event.target;
+  const data = Object.fromEntries(new FormData(form));
 
   fetch('/api/sessions', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
   })
-  .then(res => res.json())
-  .then(email => {
-    console.log(`email `, email)
-    state.loggedInUser = email})
-  .then(() => renderHomePage())
+    .then(res => res.json())
+    .then(email => {
+      console.log(`email `, email);
+      state.loggedInUser = email;
+      updateNavigationMenu(); // Update the navigation menu after successful login
+      renderHomePage();
+    });
 }
-  
+
+function logout() {
+  fetch('/api/sessions', {
+    method: 'DELETE',
+  })
+    .then(res => res.json())
+    .then(() => {
+      state.loggedInUser = '';
+      updateNavigationMenu(); // Update the navigation menu after successful logout
+      renderLogin();
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
+
+function updateNavigationMenu() {
+  const signUpLink = document.getElementById('signUpLink');
+  const loginLink = document.getElementById('loginLink');
+
+  if (state.loggedInUser) {
+    signUpLink.style.display = 'none'; // Hide the "Sign Up" option
+    loginLink.style.display = 'none'; // Hide the "Log In" option
+  } else {
+    signUpLink.style.display = 'block'; // Show the "Sign Up" option
+    loginLink.style.display = 'block'; // Show the "Log In" option
+  }
+}
